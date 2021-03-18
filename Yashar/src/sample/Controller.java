2af839a6153ddb5d3b23e04004cb95503bf7ba24
SacredRider02 <73513022+SacredRider02@users.noreply.github.com>
2021-03-19 00:33:34 +0600

    Update Controller.java

--- Yashar/src/sample/Controller.java
+++ Yashar/src/sample/Controller.java
@@ -1,4 +1,50 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    @FXML
+    private Label show;
+    private int number1;
+    private String operator = "";
+    private boolean start = true;
+    private Source source = new Source();
+
+    public void processNumbers(ActionEvent event) {
+        if (start == true){
+            show.setText("");
+            start = false;
+        }
+        String value = ((Button)event.getSource()).getText();
+        show.setText(show.getText()+value);
+    }
+
+    public void processOperators(ActionEvent event) {
+        String value = ((Button)event.getSource()).getText();
+        operator = value;
+        if (!value.equals("=")){
+            if (value.isEmpty()){
+                return;
+            }
+
+            number1 = Integer.parseInt(show.getText());
+            show.setText("");
+        }
+        else {
+            if (value.isEmpty()){
+                return;
+            }
+            int number2 = Integer.parseInt(show.getText());
+            int result = source.calculate(number1, number2, operator);
+            show.setText(String.valueOf(result));
+            operator = "";
+            number1=0;
+            number2=0;
+            start=true;
+        }
+    }
 }
